858ec01b300636cfb21ec7980fda00a12071761f
kudo17117 <mccardoza@nemsu.edu.ph>
2025-07-31 08:41:14 +0800

    update new file

--- SimpleCMSWeb/SimpleCMSWeb/Models/ModelView/AppointmentViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimpleCMSWeb.Models.ModelView
-{
-    public class AppointmentViewModel
-    {
-        public Appointment? Appointment { get; set; }
-        public string? DoctorName { get; set; }
-        public string? ClinicName { get; set; }
-    }
-}
