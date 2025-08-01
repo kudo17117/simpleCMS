e832c806bca6b57c2817b621d516cd3df1a9dbe1
kudo17117 <mccardoza@nemsu.edu.ph>
2025-08-01 08:59:08 +0800

    Final Code:)

---
+++ SimpleCMSWeb/SimpleCMSWeb/SimpleCMSWeb/Models/Appointment.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleCMSWeb.Models;
+
+public partial class Appointment
+{
+    public long Id { get; set; }
+
+    public long? Patientid { get; set; }
+
+    public long? Clinicdoctorid { get; set; }
+
+    public long? Clinicid { get; set; }
+
+    public long? Doctorid { get; set; }
+
+    public DateOnly? Consultdate { get; set; }
+
+    public TimeOnly? Consulttime { get; set; }
+
+    public string? Commentdoctor { get; set; }
+
+    public string? Commentpatient { get; set; }
+
+    [Column("created_at")]
+    public DateTime? CreatedAt { get; set; }
+
+    [Column("updated_at")]
+    public DateTime? UpdatedAt { get; set; }
+
+    [Column("deleted_at")]
+    public DateTime? DeletedAt { get; set; }
+
+    [Column("is_approved_doctor")]
+    public int IsApprovedDoctor { get; set; }
+
+    [Column("is_approved_patient")]
+    public int IsApprovedPatient { get; set; }
+
+    [Column("patient_record_id")]
+    public long? PatientRecordId { get; set; }
+
+    [Column("is_done")]
+    public int IsDone { get; set; }
+
+    public bool Issync { get; set; }
+
+    [Column("server_id")]
+    public long ServerId { get; set; }
+
+    public int Sequenceno { get; set; }
+
+    public long? Referringhciid { get; set; }
+
+    public string? Referringreason { get; set; }
+
+    public string? Bloodpressure { get; set; }
+
+    public string? Breathing { get; set; }
+
+    public string? Pulse { get; set; }
+
+    public string? Temperature { get; set; }
+
+    public string? Height { get; set; }
+
+    public string? Weight { get; set; }
+
+    public string? Bpdiastolic { get; set; }
+
+    public string? Subjective { get; set; }
+
+    public string? Objective { get; set; }
+
+
+    [NotMapped]
+    public string? Doctor { get; set; }
+
+    public virtual Clinic? Clinic { get; set; }
+
+    public virtual Patient? Patient { get; set; }
+    public virtual Clinicdoctor? Clinicdoctor { get; set; }
+
+}
---
+++ SimpleCMSWeb/SimpleCMSWeb/SimpleCMSWeb/Models/ClinicDoctor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleCMSWeb.Models;
+
+public partial class Clinicdoctor
+{
+    [Column("clinicdoctorid")]
+    public long Clinicdoctorid { get; set; }
+
+    [Column("clinicid")]
+    public long? Clinicid { get; set; }
+
+    [Column("employeeid")]
+    public long? Employeeid { get; set; }
+
+    [Column("class_id")]
+    public int? ClassId { get; set; }
+
+    [Column("best_time_to_call")]
+    public string? BestTimeToCall { get; set; }
+
+    [Column("createdbyid")]
+    public long? Createdbyid { get; set; }
+
+    [Column("created_at")]
+    public DateTime? CreatedAt { get; set; }
+
+    [Column("updatedbyid")]
+    public long? Updatedbyid { get; set; }
+
+    [Column("updated_at")]
+    public DateTime? UpdatedAt { get; set; }
+
+    [Column("deleted_at")]
+    public DateTime? DeletedAt { get; set; }
+
+    [Column("isactive")]
+    public bool? Isactive { get; set; }
+
+    [Column("patienttypeid")]
+    public long? Patienttypeid { get; set; }
+
+    [Column("clinicscheduleid")]
+    public long? ClinicScheduleId { get; set; }
+
+    [Column("averagepxperday")]
+    public int? Averagepxperday { get; set; }
+
+    // Navigation properties
+    public virtual Employee? Employee { get; set; }
+    public virtual Clinic? Clinic { get; set; }
+    public virtual ICollection<ClinicSchedule>? ClinicSchedules { get; set; }
+    public virtual ICollection<Appointment>? Appointments { get; set; }
+}
